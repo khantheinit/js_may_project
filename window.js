3bf1a9a748e6af46506b792cec341a4c421a8976
khantheinit <khanthein.1360@gmail.com>
2023-05-12 16:43:09 +0630

    12 may work pj

---
+++ window.js
@@ -0,0 +1,25 @@
+// let askedName = window.prompt("What's is your Name");
+// console.log(askedName);
+
+// adding two Number
+
+// if(window.confirm("Are you sure calculate?")){
+
+//     let num1 = window.prompt("Number 1")
+//     let num2 = window.prompt("Number 2")
+
+//     alert(Number(num1)+parseFloat(num2))
+// }
+
+
+function run(){
+    
+    let num1 = window.prompt("Number 1")
+    let num2 = window.prompt("Number 2")
+
+    alert(Number(num1)+parseFloat(num2))
+}
+//Event
+
+
+
